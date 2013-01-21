67ef37edf2d66453837b6be534e011e3a66c4575
bolid <dmitriy.grigorev@gmail.com>
2013-01-22 01:21:47 +0300

    'Activity_start_widget'

---
+++ WidgetWallPaper/src/com/example/WidgetWallPaper/ButActiv.java
@@ -0,0 +1,20 @@
+package com.example.WidgetWallPaper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 22.01.13
+ * Time: 1:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class ButActiv extends Activity {
+    @Override
+    public void onCreate (Bundle saveIns){
+        super.onCreate(saveIns);
+        setContentView(R.layout.form);
+
+    }
+}
--- WidgetWallPaper/src/com/example/WidgetWallPaper/WallPaper.java
+++ WidgetWallPaper/src/com/example/WidgetWallPaper/WallPaper.java
@@ -10,7 +10,8 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class WallPaper extends  AppWidgetProvider{
-    public static String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
+    public static String ACTION_WIDGET_BUTTON1 = "But1";
+    public  static PendingIntent button1PendingIntent = null;
     Random random = new Random();
     int i = 0;
 
@@ -22,16 +23,13 @@ public class WallPaper extends  AppWidgetProvider{
 
 
         //Подготавливаем Intent для Broadcast
-        Intent active = new Intent(context, WallPaper.class);
-        active.setAction(ACTION_WIDGET_RECEIVER);
-        active.putExtra("msg", "Hello Habrahabr");
-
-        //создаем наше событие
-        PendingIntent actionPendingIntent = PendingIntent.getActivity(context, 0, active, 0);
+        Intent button1intent = new Intent(context, ButActiv.class);
+        button1intent.setAction(ACTION_WIDGET_BUTTON1);
+        button1PendingIntent = PendingIntent.getActivity(context, 0, button1intent, 0);
 
         //регистрируем наше событие
-        remoteViews.setOnClickPendingIntent(R.id.But2, actionPendingIntent);
-        remoteViews.setTextViewText(R.id.TX, String.valueOf(random.nextInt()));
+        remoteViews.setOnClickPendingIntent(R.id.But2, button1PendingIntent);
+        remoteViews.setTextViewText(R.id.TX, "Круть");
 
 
         //обновляем виджет
@@ -43,15 +41,11 @@ public class WallPaper extends  AppWidgetProvider{
 
         //Ловим наш Broadcast, проверяем и выводим сообщение
         final String action = intent.getAction();
-        if (ACTION_WIDGET_RECEIVER.equals(action)) {
-            String msg = "null";
-            try {
-                msg = intent.getStringExtra("msg");
-            } catch (NullPointerException e) {
-                Log.e("Error", "msg = null");
-            }
-            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        if (intent.getAction().equals(ACTION_WIDGET_BUTTON1)) {
+            Toast.makeText(context, "Click",Toast.LENGTH_LONG).show();
         }
         super.onReceive(context, intent);
     }
+
+
 }
