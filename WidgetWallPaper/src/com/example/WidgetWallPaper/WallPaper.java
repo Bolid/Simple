56c2b7fa291903624d8d18479ab4e15010bbffc9
bolid <dmitriy.grigorev@gmail.com>
2013-01-29 21:35:33 +0300

    'Запуск/остановка_службы/Скачивание_фото'

--- WidgetWallPaper/src/com/example/WidgetWallPaper/WallPaper.java
+++ WidgetWallPaper/src/com/example/WidgetWallPaper/WallPaper.java
@@ -10,25 +10,35 @@ import android.widget.Toast;
 import android.util.Log;
 
 public class WallPaper extends AppWidgetProvider {
-    public static String ACTION_WIDGET_BUTTON1 = "But2";
+    public static String ACTION_WIDGET_BUTTON1 = "But1";
+    public static String ACTION_WIDGET_BUTTON2 = "But2";
     public  static PendingIntent button1PendingIntent = null;
+    public  static PendingIntent button2PendingIntent = null;
     final String TAG = "App";
 
-    /*@Override
+   /*@Override
     public  void onEnabled(Context context){
         //Подготавливаем Intent для Broadcast
 
-    } */
+
+    }      */
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        //But1
         Intent button1intent = new Intent(context, WallPaper.class);
         button1intent.setAction(ACTION_WIDGET_BUTTON1);
         button1intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
         button1PendingIntent = PendingIntent.getBroadcast(context.getApplicationContext(), 0, button1intent, 0);
+        //But2
+        Intent button2intent = new Intent(context, WallPaper.class);
+        button2intent.setAction(ACTION_WIDGET_BUTTON2);
+        button2intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+        button2PendingIntent = PendingIntent.getBroadcast(context.getApplicationContext(), 0, button2intent, 0);
         //Создаем новый RemoteViews
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
         //регистрируем наше событие
-        remoteViews.setOnClickPendingIntent(R.id.But2, button1PendingIntent);
+        remoteViews.setOnClickPendingIntent(R.id.But1, button1PendingIntent);
+        remoteViews.setOnClickPendingIntent(R.id.But2, button2PendingIntent);
         appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
         //обновляем виджет
 
@@ -37,13 +47,23 @@ public class WallPaper extends AppWidgetProvider {
     public void onReceive(Context context, Intent intent) {
         //Ловим наш Broadcast, проверяем и выводим сообщение
         final String action = intent.getAction();
-        {if (action.equals(ACTION_WIDGET_BUTTON1)) {
+        {
+            if (action.equals(ACTION_WIDGET_BUTTON1)) {
 
-                try{
+                try
+                {
                     context.startService(new Intent(context, WallService.class));
-                    Toast.makeText(context, "123", Toast.LENGTH_LONG).show();}
-                catch (Exception e){Log.e(TAG, "Ошибка вызова: ", e);}
+                }
+                catch (Exception e){Log.e(TAG, "Ошибка запуска: ", e);}
             }
+            else
+                if (action.equals(ACTION_WIDGET_BUTTON2)){
+                    try
+                    {
+                        context.stopService(new Intent(context, WallService.class));
+                    }
+                    catch (Exception e){Log.e(TAG, "Ошибка остановки: ", e);}
+                }
         }
         super.onReceive(context, intent);
     }
--- WidgetWallPaper/src/com/example/WidgetWallPaper/WallService.java
+++ WidgetWallPaper/src/com/example/WidgetWallPaper/WallService.java
@@ -3,16 +3,28 @@ package com.example.WidgetWallPaper;
 import android.app.IntentService;
 import android.content.Intent;
 import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.util.Log;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Random;
 
 
 public class WallService extends IntentService{
     final String TAG = "Serv";
+    Boolean servWork = true;
     public WallService(){
-        super("Proba");
+        super("WallService");
     }
     public void onCreate(){
         super.onCreate();
@@ -23,12 +35,53 @@ public class WallService extends IntentService{
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        Random random = new Random();
+        Bitmap bitmap = null;
         WallpaperManager wall = WallpaperManager.getInstance(getApplicationContext());
-        try {
-            wall.setResource(R.drawable.img_1);
-        } catch (IOException e) {
-            Log.e(TAG, "Ошибка: ", e);
+        String url = "";
+        String fDate = "";
+        int month = 0;
+        int day = 0;
+        int year = 0;
+        while (servWork){
+            while (month==0)
+                month = random.nextInt(13);
+            if (month == 2)
+                day = random.nextInt(28);
+            else day = random.nextInt(30);
+            year = random.nextInt(5)+2008;
+            fDate = day+"-"+month+"-"+year;
+            url = "http://api-fotki.yandex.ru/api/podhistory/poddate;"+fDate+"/?limit=1";
+            try{
+                URL conn = new URL(url);
+                InputSource inputSource = new InputSource(conn.openStream());
+                inputSource.setEncoding("UTF-8");
+                SAXParserFactory factory = SAXParserFactory.newInstance();
+                SAXParser saxParser = factory.newSAXParser();
+                XMLReader xmlReader = saxParser.getXMLReader();
+                myParser mypar = new myParser();
+                xmlReader.setContentHandler(mypar);
+                xmlReader.parse(inputSource);
+                url = mypar.url;
+                conn = new URL(url);
+                URLConnection URLcon = conn.openConnection();
+                BufferedInputStream Buf_srt = new BufferedInputStream(URLcon.getInputStream(),8192);
+                bitmap = BitmapFactory.decodeStream(Buf_srt);
+                Buf_srt.close();
+                wall.setBitmap(bitmap);
+            }
+            catch (Exception e){
+                Log.e(TAG, "Ошибка: ", e);
+            }
+            //LoadContent loadcontent = new LoadContent(url, getApplicationContext());
+            //loadcontent.execute();
+            SystemClock.sleep(3000);
         }
+        url = "";
+    }
+    public void onDestroy()
+    {
+        servWork = false;
+        super.onDestroy();
     }
 }
